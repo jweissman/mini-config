c08e64cf44fd54295bce14dda276172efcf26ef7
Joseph Weissman <jweissman1986@gmail.com>
2014-12-28 19:11:52 -0500

    add tiny implementation and specs

--- lib/simple/config.rb
+++ lib/simple/config.rb
@@ -1,1 +1,13 @@
+require 'ostruct'
 require 'simple/config/version'
+
+module Simple::Config
+  def configure
+    yield config if block_given?
+    config
+  end
+
+  def config
+    @configuration ||= OpenStruct.new
+  end
+end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -5,4 +5,37 @@ describe Simple::Config do
   it "should have a VERSION constant" do
     subject.const_get('VERSION').should_not be_empty
   end
+
+  it "should be configurable" do
+    Simple::Config.configure do |b| 
+      b.foo = "bar"  
+      b.baz = OpenStruct.new(quux: 'ilex')
+    end
+
+    expect(Simple::Config.config.foo).to eql("bar")
+    expect(Simple::Config.config.baz.quux).to eql("ilex")
+  end
+end
+
+module ExampleNamespace
+  include Simple::Config
+end
+
+describe ExampleNamespace do
+  it 'should be configurable' do
+    ExampleNamespace.config.bar = 'baz'
+    expect(ExampleNamespace.config.bar).to eql('baz')
+  end
+
+  it 'should be configurable with extended syntax' do
+    ExampleNamespace.configure do |en|
+      en.quux = 'flick'
+
+      en.x = OpenStruct.new
+      en.x.dx = 4
+    end
+    
+    expect(ExampleNamespace.config.quux).to eql('flick')
+    expect(ExampleNamespace.config.x.dx).to eql(4)
+  end
 end
